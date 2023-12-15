031c6fc4a55ed694a2820fc37b073e8ce748d384
Gabriel Castro Navarro <gabrielc.navarro@hotmail.com>
2023-12-14 23:58:07 -0300

    Configurações iniciais e conexão com Banco de Dados

---
+++ src/main/java/com/generation/loja_de_jogos/LojaDeJogosApplication.java
@@ -0,0 +1,13 @@
+package com.generation.loja_de_jogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaDeJogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaDeJogosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/loja_de_jogos/LojaDeJogosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.loja_de_jogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaDeJogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
